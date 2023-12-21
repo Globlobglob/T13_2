2214d3014b1bc8fa84564a9599b5f24a2cd08f30
Globlobglob <122725189+Globlobglob@users.noreply.github.com>
2023-12-21 15:19:08 +0200

    Add files via upload

---
+++ T13_P2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T13_P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Publish p1 = new Publish("Peace&War", "Lev", 2000);
+                Console.WriteLine(p1.ToString());
+                Console.WriteLine("Enter + year");
+            
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine(p1.Plus(x));
+                
+               
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+
+        }
+        
+
+
+        
+    }
+}
---
+++ T13_P2/Publish.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T13_P2
+{
+    class Publish
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string fam;
+
+        public string Fam
+        {
+            get { return fam; }
+            set { fam = value; }
+        }
+
+        private int year;
+
+        public int Year
+        {
+            get { return year; }
+            set { year =(value<1998||value>2023)?0:value; }
+        }
+
+
+        public Publish() { }
+        public Publish(string name,string fam,int year)
+        {
+            this.Name = name;
+            this.Fam = fam;
+            this.Year = year;
+        
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}\n{Fam}\n{Year}";
+        }
+
+
+        public int Plus(int x)
+        {
+            return x += Year;
+        }
+
+
+    }
+}
